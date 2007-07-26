7180b4e9d525ea326e34715ecfba21ff7ecefe9e
Mika Yrjölä <ext-mika.yrjola@nokia.com>
2007-07-26 06:51:23 +0000

    Version with syslog support and opensourcing preparations

--- src/sp-error-visualizer.c
+++ src/sp-error-visualizer.c
@@ -1,17 +1,42 @@
 /*
- * read stdin and show as banner messages.
+ * This file is part of sp-error-visualizer
+ *
+ * Copyright (C) 2006 Nokia Corporation. 
+ *
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 
+ * version 2 as published by the Free Software Foundation. 
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+/*
+ * reads stdin or syslog socket and shows data as banner messages.
  * Intended for showing interesting syslog messages on screen 
  * when they happen, for example GLIB criticals and such
  *
  * Compile:
- gcc `pkg-config --cflags --libs libosso` -g -Wall -O2 -o show_banner show_banner.c
+ gcc `pkg-config --cflags --libs libosso` -g -Wall -O2 -o \ 
+ sp-error-visualizer sp-error-visualizer.c
  *
  * usage example on device (your syslog file location may vary)
- tail -f /var/ftd-log/syslog | ./show_banner -f syslog_patterns &
- tail -f /var/ftd-log/syslog | ./show_banner GLIB &
+ tail -f /var/ftd-log/syslog | ./sp-error-visualizer -f syslog_patterns &
+ tail -f /var/ftd-log/syslog | ./sp-error-visualizer GLIB &
+ ./sp-error-visualizer -s -f syslog_patterns &
  * usage exmple on SDK:
- tail -f /var/ftd-log/syslog | run-standalone.sh  -f syslog_patterns GLIB &
- tail -f /var/ftd-log/syslog | run-standalone.sh ./show_banner GLIB &
+ tail -f /var/ftd-log/syslog | run-standalone.sh ./sp-error-visualizer -f syslog_patterns GLIB &
+ tail -f /var/ftd-log/syslog | run-standalone.sh ./sp-error-visualizer GLIB &
  *
  *
  Useful syslog_patterns compiled by Eero: 
@@ -56,11 +81,22 @@ GLIB ERROR
 */
 
 /* Includes */
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/syslog.h>
+#include <fcntl.h>
 #include <libosso.h>
+
+/* Hardcoding the syslog file location makes the tool fragile.
+   Can we acquire this information dynamically? */
+
+#define LOGFILE_NAME "/var/ftd-log/syslog"
 
 #define SYSLOG_MONITOR_DBUS_SERVICE "syslog_monitor"
 #define MAXMSG (256)
@@ -70,18 +106,18 @@ char buf[MAXMSG + 1];
 char prev[MAXMSG + 1];
 char *pattern[MAXPATTERNS];
 int patnum = 0;
- 
-void read_patterns(char * fname)
+
+void read_patterns(char *fname)
 {
     int len;
     FILE *f;
     f = fopen(fname, "r");
-    if ( f == NULL) { 
+    if (f == NULL) {
 	perror("Can not open patterns file:");
 	exit(1);
     }
     patnum = 0;
-    while(fgets(buf, MAXMSG, f)) {
+    while (fgets(buf, MAXMSG, f)) {
 	if (patnum >= MAXPATTERNS) {
 	    return;
 	}
@@ -96,8 +132,8 @@ void read_patterns(char * fname)
 	if (len > 0 && buf[len - 1] == '\n') {
 	    buf[len - 1] = 0;
 	}
-	pattern[patnum] = (char *)malloc(len + 1);
-	if(!pattern[patnum]) {
+	pattern[patnum] = (char *) malloc(len + 1);
+	if (!pattern[patnum]) {
 	    g_print("read_patterns: Can not allocate memory\n");
 	    exit(1);
 	}
@@ -106,28 +142,81 @@ void read_patterns(char * fname)
 	patnum++;
     }
     fclose(f);
-    g_print("read_patterns: processed %d patterns from file %s\n", patnum, fname);
+    g_print("read_patterns: processed %d patterns from file %s\n", patnum,
+	    fname);
 }
 
 int main(int argc, char *argv[])
 {
     osso_context_t *osso_context;
     int numspaces, len;
+    char *patternfile = NULL;
     char *p, *end;
     int pn, pattern_found;
+    int slog_socket = -1, readfromfile = 0;
+    int optchar;
 
-    if (argc > 1) {
-	if (!strcmp(argv[1] , "-f")) {
-	    if (argc != 3) {
-		g_print("if giving pattern filename, must use exactly two arguments: %s -f FILENAME\n", argv[0]);
+    struct sockaddr slog_socket_addr;
+    fd_set fds;
+
+    while ((optchar = getopt(argc, argv, "sf:")) != -1) {
+	switch (optchar) {
+
+	case 's':
+
+	    /* The check below might fail for other reasons than the
+	       non-existence of the file. However, in that case reading
+	       from the file via piping to stdin probably fails too */
+
+	    if ((access(LOGFILE_NAME, F_OK) == 0)) {
+		g_print
+		    ("Won't read from socket as syslog appears to exist.\n");
+		g_print("See documentation for more details.\n");
 		return 1;
 	    }
-	    read_patterns(argv[2]);
-	} else {
-	    for (;patnum < (argc - 1) && patnum < MAXPATTERNS; patnum++) {
-		pattern[patnum] = argv[patnum + 1];
-		g_print("pattern[%3d]: [%s]\n", patnum, pattern[patnum]);
+
+	    /* Ensure that any previous syslog sockets are not dangling around */
+
+	    unlink(_PATH_LOG);
+
+	    slog_socket_addr.sa_family = AF_UNIX;
+	    strncpy(slog_socket_addr.sa_data, _PATH_LOG,
+		    sizeof(slog_socket_addr.sa_data));
+	    slog_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+
+	    if (bind(slog_socket, &slog_socket_addr,
+		     sizeof(slog_socket_addr.sa_family) +
+		     strlen(slog_socket_addr.sa_data)) == -1) {
+		g_warning("Syslog socket invocation failed!\n");
+		perror("Reason was");
+		return (1);
 	    }
+
+	    /* Setup permissions to allow everybody r/w access to log */
+	    if (chmod(_PATH_LOG, 0666) < 0) {
+		g_warning("Could not setup syslog socket permissions\n");
+		return 1;
+	    }
+
+	    break;
+
+	case 'f':
+	    patternfile = (char *) optarg;
+	    read_patterns(patternfile);
+	    readfromfile = 1;
+	    break;
+
+	default:
+	    g_print
+		("Please look at the documentation for usage information\n");
+	    return 1;
+	}
+    }
+
+    if (!readfromfile && optind < argc) {
+	for (; patnum < (argc - 1) && patnum < MAXPATTERNS; patnum++) {
+	    pattern[patnum] = argv[optind + patnum];
+	    g_print("pattern[%3d]: [%s]\n", patnum, pattern[patnum]);
 	}
     }
 
@@ -138,17 +227,44 @@ int main(int argc, char *argv[])
 	return 1;
     }
 
+    /* Using fdopen and fcntl/setsockopts might allow using just fgets
+       for both socket and piped input, but the below approach is
+       probably safer */
+
     while (1) {
-	if (!fgets(buf, MAXMSG, stdin)) {
-	    break;
+
+	memset(&buf, '\0', sizeof(buf));
+
+	if (slog_socket < 0) {
+	    if (!fgets(buf, MAXMSG, stdin)) {
+		break;
+	    }
+	} else {
+	    FD_ZERO(&fds);
+	    FD_SET(slog_socket, &fds);
+	    if (select(slog_socket + 1, &fds, NULL, NULL, NULL) < 0) {
+		g_print("Select failed. Exiting...\n");
+		close(slog_socket);
+		return 1;
+	    }
+	    if (FD_ISSET(slog_socket, &fds)) {
+		int ret;
+		ret = recv(slog_socket, buf, MAXMSG - 1, 0);
+		if (ret < 0) {
+		    g_print
+			("Reading from the syslog socket failed. Exiting...\n");
+		    return 1;
+		}
+	    }
 	}
 	p = buf;
 	end = buf + MAXMSG;
+
 	/*
 	 * poor man parser to ignore prefix of typical syslog line:
 	 *
-May 16 12:53:44 Nokia-N800-19 iap_conndlg 1.3.51[1915]: NameownerChanged(:1.1278, , :1.1278)
-         *
+	 May 16 12:53:44 Nokia-N800-19 iap_conndlg 1.3.51[1915]: NameownerChanged(:1.1278, , :1.1278)
+	 *
 	 * we use part starting after 4th space
 	 */
 	for (numspaces = 0; p < end && numspaces < 4; p++) {
@@ -163,7 +279,7 @@ May 16 12:53:44 Nokia-N800-19 iap_conndlg 1.3.51[1915]: NameownerChanged(:1.1278
 	    continue;
 	}
 	for (pattern_found = pn = 0; pn < patnum; pn++) {
-	    if(strstr(p, pattern[pn])) {
+	    if (strstr(p, pattern[pn])) {
 		pattern_found = 1;
 		break;
 	    }
@@ -171,7 +287,7 @@ May 16 12:53:44 Nokia-N800-19 iap_conndlg 1.3.51[1915]: NameownerChanged(:1.1278
 	if (patnum && !pattern_found) {
 	    continue;
 	}
-	
+
 	strncpy(prev, p, MAXMSG);
 	len = strlen(p);
 	if (p[len - 1] == '\n') {
@@ -180,7 +296,6 @@ May 16 12:53:44 Nokia-N800-19 iap_conndlg 1.3.51[1915]: NameownerChanged(:1.1278
 	osso_system_note_infoprint(osso_context, p, NULL);
     }
 
-    osso_deinitialize (osso_context);
+    osso_deinitialize(osso_context);
     return 0;
 }
-
