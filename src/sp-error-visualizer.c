ee35767f5de08b67196ef399509ebe2017bf114f
Andris Zeila <andris.zeila@accenture.com>
2011-03-29 20:51:07 +0100

    Replaced osso notifications with org.freedesktop.Notifications interface.

--- src/sp-error-visualizer.c
+++ src/sp-error-visualizer.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of sp-error-visualizer
  *
- * Copyright (C) 2007 Nokia Corporation. 
+ * Copyright (C) 2007,2011 Nokia Corporation.
  *
  * Contact: Eero Tamminen <eero.tamminen@nokia.com>
  *
@@ -27,7 +27,7 @@
  * when they happen, for example GLIB criticals and such
  *
  * Compile:
- gcc `pkg-config --cflags --libs libosso` -g -Wall -O2 -o \ 
+ gcc `pkg-config --cflags --libs dbus-glib-1` -g -Wall -O2 -o \
  sp-error-visualizer sp-error-visualizer.c
  *
  * usage examples on device (your syslog file location may vary)
@@ -94,7 +94,8 @@ GLIB ERROR
 #include <sys/inotify.h>
 #include <sys/time.h>
 #include <fcntl.h>
-#include <libosso.h>
+#include <glib.h>
+#include <dbus/dbus-glib.h>
 
 #define _GNU_SOURCE
 #include <getopt.h>
@@ -169,7 +170,6 @@ int add_logfile_creation_monitor(FILE * logfile, char *logfilepath,
 
 int main(int argc, char *argv[])
 {
-    osso_context_t *osso_context;
     int numspaces, len;
     char *patternfile = NULL, *logfilepath = NULL;
     char *p, *end;
@@ -286,12 +286,22 @@ int main(int argc, char *argv[])
 	}
     }
 
-    osso_context = osso_initialize(SYSLOG_MONITOR_DBUS_SERVICE,
-				   "0.1", FALSE, NULL);
-    if (!osso_context) {
-	g_print("Error doing osso_initialize()\n");
-	return 1;
+    /* setup dbus-glib required stuff */
+    g_type_init();
+    GError *error = NULL;
+    DBusGConnection *connection = dbus_g_bus_get (DBUS_BUS_SESSION, &error);
+    if (connection == NULL) {
+	g_printerr ("Failed to open connection to bus: %s\n", error->message);
+	g_error_free (error);
+	exit (1);
     }
+    DBusGProxy *proxy = dbus_g_proxy_new_for_name (connection,
+	"org.freedesktop.Notifications",
+	"/org/freedesktop/Notifications",
+	"org.freedesktop.Notifications"
+	);
+    GHashTable *hash = g_hash_table_new (g_str_hash, g_str_equal);
+    /**/
 
     while (1) {
 
@@ -416,10 +426,21 @@ int main(int argc, char *argv[])
 	if (p[len - 1] == '\n') {
 	    p[len - 1] = 0;
 	}
-	osso_system_note_infoprint(osso_context, p, NULL);
+    dbus_g_proxy_call_no_reply(proxy, "Notify",
+		G_TYPE_STRING, "",
+		G_TYPE_UINT, 0,
+		G_TYPE_STRING, "",
+		G_TYPE_STRING, "Error",
+		G_TYPE_STRING, p,
+		G_TYPE_STRV, NULL,
+		dbus_g_type_get_map("GHashTable", G_TYPE_STRING, G_TYPE_VALUE), hash,
+		G_TYPE_INT, -1,
+		G_TYPE_INVALID
+		);
     }
+    g_hash_table_destroy (hash);
+    g_object_unref(proxy);
 
-    osso_deinitialize(osso_context);
     if (inotify_fd > 0) {
 	close(inotify_fd);
     }
